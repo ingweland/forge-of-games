dfb209272ebf91cbfa35e614c015a210a4cd4816
Ingweland <ingweland@proton.me>
2026-07-15 10:20:13 +0200

    Add JsonIgnore attributes to AllianceWoaRankingDto properties

--- src/Dtos.Hoh/Stats/AllianceWoaRankingDto.cs
+++ src/Dtos.Hoh/Stats/AllianceWoaRankingDto.cs
@@ -1,15 +1,31 @@
+using System.Text.Json.Serialization;
 using Ingweland.Fog.Models.Hoh.Enums;
 
 namespace Ingweland.Fog.Dtos.Hoh.Stats;
 
 public class AllianceWoaRankingDto
 {
+    [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
     public required int DivisionId { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
     public required int EloDelta { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
     public required int EloRating { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
     public required DateTime EndedAt { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
     public required int EventId { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
     public required DateTime StartedAt { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
     public required WoaTier Tier { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
     public required int VictoryPoints { get; init; }
 }
